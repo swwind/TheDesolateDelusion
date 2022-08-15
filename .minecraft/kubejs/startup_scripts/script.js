78835f368d89fad7f3704c1b2cab45c4c712a083
swwind <swwind233@gmail.com>
2022-08-15 19:35:54 +0800

    save work

--- .minecraft/kubejs/startup_scripts/script.js
+++ .minecraft/kubejs/startup_scripts/script.js
@@ -1,9 +1,18 @@
 // priority: 0
 
-console.info('Thanks for playing The Desolute Delusion!')
+console.info("Thanks for playing The Desolute Delusion!");
 
-onEvent('item.registry', event => {
-  event
-    .create('bilibili_television')
-    .rarity('epic')
-})
+onEvent("item.registry", (event) => {
+  event.create("bilibili_television").rarity("epic");
+
+  event.create("pyro_crystal").rarity("uncommon");
+  event.create("hydro_crystal").rarity("uncommon");
+  event.create("electro_crystal").rarity("uncommon");
+  event.create("cryo_crystal").rarity("uncommon");
+  event.create("dendro_crystal").rarity("uncommon");
+  event.create("anemo_crystal").rarity("uncommon");
+  event.create("geo_crystal").rarity("uncommon");
+
+  event.create("primogem").rarity("rare");
+  event.create("intertwined_fate").rarity("rare");
+});
