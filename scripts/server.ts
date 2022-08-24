75b13a8c2a18ba8f0db36c980208668c4a39d6d8
swwind <swwind233@gmail.com>
2022-08-24 18:35:03 +0800

    233

--- scripts/server.ts
+++ scripts/server.ts
@@ -3,7 +3,7 @@ import { $ } from "https://deno.land/x/zx_deno@1.2.2/mod.mjs";
 await $`rm -rf server`;
 await $`mkdir -p server`;
 
-await $`if [ ! -f forge-1.18.2-40.1.73-installer.jar ]; then wget -O installer.jar https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.73/forge-1.18.2-40.1.73-installer.jar; fi`;
+await $`if [ ! -f forge-1.18.2-40.1.73-installer.jar ]; then wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.18.2-40.1.73/forge-1.18.2-40.1.73-installer.jar; fi`;
 await $`java -jar forge-1.18.2-40.1.73-installer.jar --installServer server`;
 
 await $`mkdir -p server/mods`;
